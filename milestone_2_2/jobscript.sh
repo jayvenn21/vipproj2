86d26c7a5ab13d00af108877f4ee3669aae580c8
BLee654 <leeben8654@gmail.com>
2025-04-27 10:09:26 -0400

    found what was wrong with qa

--- milestone_2_2/jobscript.sh
+++ milestone_2_2/jobscript.sh
@@ -16,7 +16,7 @@ case $TASK in
   ("Translate")
     srun python ~/vipproj2/milestone_2_2/hpc_translate.py 
     ;;
-  ("Answer Question")
+  ("'Answer Question'")
     srun python ~/vipproj2/milestone_2_2/hpc_qa.py 
     ;;
   ("Classify")
